df22e169dcbbdd1e0ad1300bb877e0c9c9179876
bhanudova02 <bhanudova02@gmail.com>
2025-02-18 14:13:31 +0530

    new changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import SignUp from "./components/SignUp";
-import Login from "./components/Login";
-import Home from "./components/Home"; // Optional Home Page
-import Demo from "./components/Demo";
-
 function App() {
   return (
     <div>
-      <Demo/>
+   
     </div>
   );
 }
